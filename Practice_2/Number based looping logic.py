c8f861fbf2a5ae455c5dfb64d4292a2becb4fa9f
Shoaib Ali <Mohdshoaibaliii@gmail.com>
2026-07-16 07:42:26 +0530

    Number based looping 63 completed

--- Practice_2/Number based looping logic.py
+++ Practice_2/Number based looping logic.py
@@ -22,3 +22,20 @@ while num>0:
     rev=rev*10+dig
     num//=10
 print(rev)
+
+#63.Check if given number is palindrome
+num=int(input("Enter a Number: "))
+temp=num
+rev=0
+while num>0:
+    dig=num%10
+    rev=rev*10+dig
+    num//=10
+print("Palindrome"  if temp==rev else "Not Palindrome")
+
+#================================================
+num=int(input("Enter a Number: "))
+rev=int(str(num)[::-1])
+
+print("P" if num==rev else "NP")
+
