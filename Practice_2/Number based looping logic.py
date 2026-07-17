e80ece76b4efe9960ca3bcb590294988c12ff98c
Shoaib Ali <Mohdshoaibaliii@gmail.com>
2026-07-17 23:26:36 +0530

    Number based looping 67 completed

--- Practice_2/Number based looping logic.py
+++ Practice_2/Number based looping logic.py
@@ -71,3 +71,13 @@ if summ==num:
     print("Perfect number")
 else:
     print("Not a Perfect Number")
+
+
+#67.print all prime numbers between 1 to 100
+num =int(input())
+for n in range(2,num+1):
+    for i in range(2,int(n**0.5)+1):
+        if n%i ==0:
+            break
+    else:
+        print(n,end=" ")
